9e889fc7d34ea5027569b1d22efc85f939565323
Jonathan Sidi <yonis@metrumrg.com>
2018-12-11 11:51:53 -0500

    update DESCRIPTION with unttaged version of GH texPreview

--- R/pad_col.R
+++ R/pad_col.R
@@ -1,16 +1,41 @@
 #' @title pad columns of texblock
 #' @description add columns to texblock to the left
 #' @param x tb
-#' @param cols number of columns to pad, Default: 1
+#' @param cols numeric, number of columns to pad, Default: 1
+#' @param side numeric, which side to pad, 1='left' 2='right', Default: 1
 #' @return tb
 #' @rdname pad_col
 #' @export 
 
-pad_col <- function(x,cols=1){
-  ret <- sprintf('%s%s',
-          strrep('&',cols),
-          strsplit(x,'\n')[[1]])
-  as.tb(paste0(ret,collapse = '\n'))
+pad_col <- function(x, cols = 1, side = 1){
+  
+  if(!side%in%c(1,2))
+    stop('side must be either 1 (left) or 2 (right)')
+  
+  old_text <- strsplit(x,'\n')[[1]]
+  idx <- grepl('\\\\$',old_text)
+  new_text <- gsub('\\\\$','',old_text)
+  
+  if(side==1){
+    new_text <- sprintf('%s%s',
+                        strrep('&',cols),
+                        new_text
+                        )
+  }
+  
+  if(side==2){
+    new_text <- sprintf('%s%s',
+                        new_text,
+                        strrep('&',cols)
+                        )
+  }
+  
+  new_text[idx] <- sprintf('%s\\\\',new_text[idx])
+
+  
+  out_text <- paste0(new_text,collapse = '\n')
+  
+  as.tb(out_text)
   
 }
 
