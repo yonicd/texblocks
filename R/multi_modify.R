cdb09c9c0351038a751b30ad308a024973aff5aa
Jonathan Sidi <yonis@metrumrg.com>
2019-01-05 22:44:31 -0500

    add functions to modify multirow/multicol

---
+++ R/multi_modify.R
@@ -0,0 +1,47 @@
+modify_multirow <- function(tb,i,j,new){
+  
+  modify_multi(tb,i,j,new,'MULTIROW',find_multirow)
+  
+}
+
+modify_multicol <- function(tb,i,j,new){
+  
+  modify_multi(tb,i,j,new,'MULTICOL',find_multicol)
+  
+}
+
+modify_multi <- function(tb,i,j,new,type,f){
+  
+  new <- as.character(new)
+  
+  attr_env <- new.env()
+  
+  x <- strip(tb,attr_env)
+  
+  x <- as.data.frame(x)
+  
+  obj <- attr_env[[type]]
+  
+  err_msg <- sprintf('no %s found in row %s, column %s',tolower(type),i,j)
+  
+  if(nrow(obj)==0){
+    stop(err_msg)
+  }
+  
+  idx <- which(as.numeric(obj$row)==i&as.numeric(obj$col)==j)
+  
+  if(length(idx)==0){
+    stop(err_msg)
+  }
+  
+  new_obj <- f(new)
+  
+  obj$n[idx] <- new_obj$n
+  obj$old_val[idx] <- new_obj$old_val
+  
+  attr_env[[type]] <- obj
+  
+  ret <- restore(x,attr_env)
+  
+  as.tb(ret)
+}
